9f3fe1016435ce24bf67c3d11b9e6dd6a218c26b
jackpot1022 <shin_1214@naver.com>
2023-12-14 04:32:04 +0900

    BFS 최신화

--- TrapDefense/BFS.h
+++ TrapDefense/BFS.h
@@ -2,16 +2,6 @@
 
 #include "ProgramBase.h"
 
-
-
-// 변수 선언
-static int DIRECTION_OFFSETS[NUM_DIRECTIONS][2] = {
-	{0, -1},		// 0 (상)
-	{1, 0},			// 1 (우)
-	{0, 1},			// 2 (하)
-	{-1, 0}			// 3 (좌)
-};
-
 //맵의 좌표
 
 enum PosStatus { NOT_VISIT = 0, VISIT = 1 };
